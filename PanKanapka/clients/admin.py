82d049e7d416802ffa5de99d004c1ee4369db343
Aleksandra <aleksapos@gmail.com>
2019-04-29 18:29:19 +0200

    Base client and organization

---
+++ PanKanapka/clients/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .forms import UserAdminCreationForm, UserAdminChangeForm
+from .models import Client
+
+
+class ClientAdmin(BaseUserAdmin):
+    form = UserAdminChangeForm
+    add_form = UserAdminCreationForm
+
+    list_display = ('email', 'name', 'group', 'surname', 'active', 'staff')
+    list_filter = ('admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'name', 'group', 'surname', 'active', 'staff')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'group', 'password1', 'password2', 'name', 'surname', 'active', 'staff')}
+        ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(Client, ClientAdmin)
---
+++ PanKanapka/clients/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+
+
+class ClientManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_staffuser(self, email, password):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.staff = True
+        user.admin = True
+        user.save(using=self._db)
+        return user
+
+
+class Client(AbstractBaseUser):
+    email = models.CharField(max_length=100, default=None, unique=True)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    active = models.BooleanField(default=True)
+    group = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
+    staff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_active(self):
+        return self.active
+
+    objects = ClientManager()
