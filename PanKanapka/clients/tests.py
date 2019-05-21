35a20982124a31d0c18f0720b3c2002a86151e2e
Tomczak <jacek.tomczak-ext@man.eu>
2019-05-21 14:35:13 +0200

    dodano test sprawdzajacy zapis zadan POST do db

--- PanKanapka/clients/tests.py
+++ PanKanapka/clients/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from django.core.exceptions import ValidationError
 from .models import User
+from .views import registration_form
 
 # Create your tests here.
 
@@ -19,6 +22,27 @@ class CreateUserTest(TestCase):
         second_user.surname = 'second_surname'
         second_user.active = False
         second_user.save()
-
         saved_users = User.objects.all()
         self.assertEqual(saved_users.count(), 2)
+
+    def test_website_handle_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['name'] = 'name'
+        response = registration_form(request)
+
+    def test_website_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['email'] = 'ppp@pp.pl'
+        request.POST['name'] = 'ppp'
+        response = registration_form(request)
+        self.assertEqual(User.objects.all().count(), 1)
+
+
+class ListAndModelTest(TestCase):
+    def test_cannot_save_empty_record(self):
+        user = User.objects.create(email='jacek')
+        with self.assertRaises(ValidationError):
+            user.save()
+            user.full_clean()
--- PanKanapka/clients/views.py
+++ PanKanapka/clients/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from .models import User
 
 
 def logout_view(request):
@@ -15,3 +16,9 @@ def login_view(request):
     messages.info(request, "Zostałeś poprawnie zalogowany")
     next_page = request.POST.get('next', '/')
     return HttpResponseRedirect(next_page)
+
+
+def registration_form(request):
+    if request.method == 'POST':
+        user = User(email='ppp@pp.pl', name='ppp')
+        user.save()
