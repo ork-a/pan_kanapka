932051bc33bf0ff423c80d0fdffc6636cac3e939
MyName <test@testttt.pl>
2019-05-25 14:44:16 +0200

    wyglad

--- PanKanapka/PanKanapka/settings.py
+++ PanKanapka/PanKanapka/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -88,6 +89,7 @@ DATABASES = {
     }
 }
 
+print(os.path.join(BASE_DIR, 'db.sqlite3'))
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -127,6 +129,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [(os.path.join(BASE_DIR, "static")),
+                     ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static_root")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'sandwiches/media')
