8c8526f3db4eee1f96fd0ab5a84ed3f879bd2d58
h5326 <jacek.tomczak-ext@man.eu>
2019-06-01 12:09:23 +0200

    dodano test logowania uzytkownika, czas na test po zalogowaniu

--- PanKanapka/functional_test/test_TC1.py
+++ PanKanapka/functional_test/test_TC1.py
@@ -1,22 +1,38 @@
 from selenium import webdriver
-from django.test import LiveServerTestCase
+from selenium.webdriver.common.keys import Keys
+from unittest import TestCase
 
 from time import sleep
 
+WEB_ADDRESS = 'http://127.0.0.1:8000'
 
-class RegistrationTest(LiveServerTestCase):
+class RegistrationTest(TestCase):
     #Użytkownik otwiera przeglarkę
     def setUp(self):
         self.browser = webdriver.Firefox()
     # Użytkownik wchodzi na stronę główną
     def test_get_registration_site(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(WEB_ADDRESS)
         # Użytkownik klika zaloguj
-        sleep(20)
-        self.browser.find_elements
+        self.browser.find_element_by_link_text('Login').click()
+        # Użytkownik jest przeniesiony na stronę logowania
+        header_text = self.browser.find_element_by_tag_name('h4').text
+        self.assertIn('Podaj email i hasło', header_text)
 
-    # Użytkownik jest przeniesiony na stronę logowania
-    # Użytkownik wpisuje dane do logowania (email, hasło) i klika zaloguj
+        # Użytkownik wpisuje dane do logowania (email, hasło) i klika zaloguj
+        input_user_id = self.browser.find_element_by_id('id_username')
+        input_user_id.send_keys('test@test.pl')
+        input_password_id = self.browser.find_element_by_id('id_password')
+        sleep(5)
+        self.assertEqual('password', input_password_id.get_attribute('type'))
+        input_password_id.send_keys('test')
+        input_password_id.send_keys(Keys.ENTER)
+
+        sleep(20)
+
+
+
+
     # Przeniesienie na stronę wyboru kanapki
     # Użytkownik klika wyloguj
     # Przeniesienie na stronę główną, wyboru kanapki, bez użytkownika
