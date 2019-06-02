6bc3d2e77c31cc56c564a125a6adb336c95fee32
h5326 <jacek.tomczak-ext@man.eu>
2019-06-02 10:04:16 +0200

    test widoku zokonczyl sie niepowodzeniem, najpierw trzeba polaczyc sie z plus_minus

--- PanKanapka/sandwiches/tests.py
+++ PanKanapka/sandwiches/tests.py
@@ -1,6 +1,16 @@
 from django.test import TestCase
+from django.http.request import HttpRequest
+
+from .views import sandwiches
 
 # Create your tests here.
 class ListSandwichTest(TestCase):
     def setUp(self):
         pass
+
+    def test_sandiwch_view(self):
+        request = HttpRequest()
+        request.method = 'GET'
+
+        response = sandwiches(request)
+        self.assertTemplateUsed(response, "sandwiches_list.html")
